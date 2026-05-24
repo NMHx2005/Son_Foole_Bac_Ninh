f5ba457400dad599af0f41ea799fb014742e77f7
Nguyen Hung <22092005nguyenhung@gmail.com>
2026-05-24 14:16:33 +0700

    include sqlite db in vercel bundle

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ["better-sqlite3"],
+  outputFileTracingIncludes: {
+    "/*": ["./storage/prices.sqlite"],
+  },
 };
 
 export default nextConfig;
